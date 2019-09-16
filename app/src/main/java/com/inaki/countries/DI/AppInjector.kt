103b2dbb760ac2471c1453669238148d7c0beafc
inaki92 <lizarraga9206@gmail.com>
2019-09-16 12:16:58 -0400

    Setting up the model, and the network connection

---
+++ app/src/main/java/com/inaki/countries/DI/AppInjector.kt
@@ -0,0 +1,2 @@
+package com.inaki.countries.DI
+
---
+++ app/src/main/java/com/inaki/countries/ViewModel/CountriesViewModel.kt
@@ -0,0 +1,9 @@
+package com.inaki.countries.ViewModel
+
+import androidx.lifecycle.ViewModel
+import com.inaki.countries.Network.CountryService
+
+class CountriesViewModel(private val countryService: CountryService): ViewModel() {
+
+
+}
