f4c5fbbdd01a2fd33d3cc5f797670c546e647ca2
inaki92 <lizarraga9206@gmail.com>
2019-09-16 12:32:51 -0400

    Setting up Dependency injection with Koin and the splash screen

--- app/src/main/java/com/inaki/countries/CountriesApp.kt
+++ app/src/main/java/com/inaki/countries/CountriesApp.kt
@@ -1,6 +1,19 @@
 package com.inaki.countries
 
 import android.app.Application
+import com.inaki.countries.DI.networkModule
+import com.inaki.countries.DI.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class CountriesApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@CountriesApp)
+            modules(listOf(networkModule, viewModelModule))
+        }
+    }
 }
